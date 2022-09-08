e5efe22c5cb3ff17edd72690af7846a6dd1e4736
RomanSchmid <rrr.schmid@gmail.com>
2022-09-08 23:14:01 +0200

    feat: optional city

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-const fetchURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Praha?unitGroup=metric&key=WD49RMATTW3ARZ4FQ3AQAC239&contentType=json";
-
 const temperature = document.querySelector(".temperature");
 const feelsLike = document.querySelector(".feels-like");
 const humidity = document.querySelector(".humidity");
 const pressure = document.querySelector(".pressure");
 const uvIndex = document.querySelector(".uv-index");
 
-const city = document.querySelector(".container h1");
-const cityInfo = document.querySelector(".container p");
-const forecast = document.querySelector(".container h2");
+const city = document.getElementById("city")
+const cityInfo = document.getElementById("city-info");
+const forecast = document.getElementById("forecast");
 
 const refreshTime = document.getElementById("refresh-time");
 let timer = 1;
 
+let timeUpdate;
+function resetTimer() {
+  clearInterval(timeUpdate);
+  refreshTime.innerText = "";
+  timeUpdate = setInterval(() => {
+    refreshTime.innerText = `Last update ${timer} minutes ago`;
+    timer++;
+  }, 60000);
+}
+
 function getWeather() {
-  console.log("test");
-  fetch(fetchURL)
+  resetTimer();
+  fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + city.innerText + "?unitGroup=metric&key=WD49RMATTW3ARZ4FQ3AQAC239&contentType=json")
 	.then(response => response.json())
 	.then(response => {
+    /* console.log(response); */
     city.innerText = response.address;
     cityInfo.innerText = response.resolvedAddress;
     forecast.innerText = response.description;
@@ -34,16 +43,29 @@ function getWeather() {
   timer = 1;
 }
 
-const timeUpdate = setInterval(() => {
-  refreshTime.innerText = `Last update ${timer} minutes ago`;
-  timer++;
-}, 60000);
-
 getWeather();
 setInterval(getWeather, 300000);
 
 const btn = document.getElementById("btn");
 btn.addEventListener("click", () => {
-  refreshTime.innerText = "";
   getWeather();
 });
+
+const anotherCity = document.querySelector(".another-city");
+const contentToHide = document.querySelector(".content-to-hide");
+//Display optional city and hide everything else
+city.addEventListener("click", () => {
+  anotherCity.classList.toggle("display-block");
+  contentToHide.classList.toggle("display-none");
+})
+
+//Toggle between cities
+anotherCity.addEventListener("click", () => {
+  let curCity = city.innerText;
+  let newCity = anotherCity.innerText;
+  city.innerText = newCity;
+  anotherCity.innerText = curCity;
+  anotherCity.classList.toggle("display-block");
+  contentToHide.classList.toggle("display-none");
+  getWeather();
+})
